cfe4995f67e7164d786395bcd94fe383132f102a
HaydenHartley <hhart30@hotmail.com>
2017-08-04 10:41:46 +1000

    Finish Prac01

---
+++ ProgrammingExercises/Comments.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+/**
+ * Created by jc428741 on 4/08/17.
+ * Program comments are nonexecuting statements you add to a file for the purpose of documentation
+ */
+public class Comments {
+    /* Program comments are nonexecuting statements you add to a file for the purpose of documentation */
+    public static void main(String[] args) {
+        //Program comments are nonexecuting statements you add to a file for the purpose of documentation
+        JOptionPane.showMessageDialog(null, "Program comments are nonexecuting statements you add to a file for the purpose of documentation");
+    }
+}
---
+++ ProgrammingExercises/MovieQuote.java
@@ -0,0 +1,9 @@
+/**
+ * Created by jc428741 on 4/08/17.
+ *
+ */
+public class MovieQuote {
+    public static void main(String[] args) {
+        System.out.println("\"I'm Dale, but you can call me Dragon\"");
+    }
+}
